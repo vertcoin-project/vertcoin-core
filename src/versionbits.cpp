357581baf1d9553ea899148cd1b02381fb94cb73
James Lovejoy <jameslovejoy1@gmail.com>
2017-02-13 19:21:14 -0500

    Fix incorrect array order for BIP9 forks

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -12,16 +12,16 @@ const struct BIP9DeploymentInfo VersionBitsDeploymentInfo[Consensus::MAX_VERSION
         /*.gbt_force =*/ true,
     },
     {
-        /*.name =*/ "nversionbips",
-        /*.gbt_force =*/ true,
-    },
-    {
         /*.name =*/ "csv",
         /*.gbt_force =*/ true,
     },
     {
         /*.name =*/ "segwit",
         /*.gbt_force =*/ false,
+    },
+    {
+        /*.name =*/ "nversionbips",
+        /*.gbt_force =*/ true,
     }
 };
 
