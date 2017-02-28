5c84e90b760bc42136a711e693965718224b5f18
James Lovejoy <jameslovejoy1@gmail.com>
2017-02-28 00:38:09 -0500

    Use same RPC port as old VTC by default

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 8332;
+        nRPCPort = 5888;
     }
 };
 static CBaseMainParams mainParams;
