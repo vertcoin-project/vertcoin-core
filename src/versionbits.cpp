74626329e8ed6998d7b4a13237c888dcb0603b38
James Lovejoy <jameslovejoy1@gmail.com>
2017-11-12 21:31:02 -0500

    No BIP9 test on VTC

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -7,10 +7,6 @@
 
 const struct VBDeploymentInfo VersionBitsDeploymentInfo[Consensus::MAX_VERSION_BITS_DEPLOYMENTS] = {
     {
-        /*.name =*/ "testdummy",
-        /*.gbt_force =*/ true,
-    },
-    {
         /*.name =*/ "csv",
         /*.gbt_force =*/ true,
     },
