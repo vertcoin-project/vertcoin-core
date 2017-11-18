e61a307121a5e1e689b152a02aef6e080a72f584
James Lovejoy <jameslovejoy1@gmail.com>
2017-11-18 18:03:30 -0500

    Add Eric's DNS seed

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -128,6 +128,7 @@ public:
         vSeeds.emplace_back("vtc.gertjaap.org", true); // Gert-Jaap Glasbergen
         vSeeds.emplace_back("seed.vtc.bryangoodson.org", true); // Bryan Goodson
         vSeeds.emplace_back("dnsseed.pknight.ca", true); // pknight
+        vSeeds.emplace_back("seed.orderofthetaco.org", true); // Eric (etang600)
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,71);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
