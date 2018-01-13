a047821e35e37035014ecf3f83d84da37066e6ac
Lukas-Christian Schulze <lukas.schulze@gmail.com>
2018-01-13 14:41:26 +0100

    ADDED: Copyright lines
    First commit for Verticalcoin.

--- src/arith_uint256.cpp
+++ src/arith_uint256.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2016 The Bitcoin Core developers
 // Copyright (c) 2014-2017 The Vertcoin Developers
+// Copyright (c) 2014-2018 The Verticalcoin Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
