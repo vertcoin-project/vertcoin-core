140f753ba3eb3ac7dbb8a61afa483898177a7c9d
James Lovejoy <jameslovejoy1@gmail.com>
2018-04-06 02:48:52 -0400

    fix undefined std::pow on mac

--- src/pow.cpp
+++ src/pow.cpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2009-2017 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#include <cmath>
 
 #include <pow.h>
 
