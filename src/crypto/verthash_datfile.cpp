da2f6431a9f647665c72b7b0014c2b3c2913070b
Gert-Jaap Glasbergen <gertjaap@gertjaap.org>
2019-05-30 13:28:05 +0200

    Changes in file handling logic for Windows

--- src/crypto/verthash_datfile.cpp
+++ src/crypto/verthash_datfile.cpp
@@ -30,50 +30,56 @@ void VerthashDatFile::UpdateMiningDataFile() {
 
     FILE* blockFile;
     uint256 prevBlockHash;
+    int startPos = 0;
+
+    unsigned char prev_row[VERTHASH_MULTIPLICATION][32], curr_row[VERTHASH_MULTIPLICATION][32], double_buf[64];
+
+    memset(prev_row, 0, sizeof(prev_row));
 
     if(boost::filesystem::exists(targetFile)) {
         blockFile = fsbridge::fopen(targetFile.c_str(),"rb+");
-    } else {
-        blockFile = fsbridge::fopen(targetFile.c_str(),"wb+");
-    }
 
-    CAutoFile filein(blockFile, SER_DISK, CLIENT_VERSION);
-    fseek(blockFile, 0, SEEK_END);
 
-    // Find common ancestor
-    while(ftell(blockFile) >= VERTHASH_MULTIPLICATION*32){
+        CAutoFile filein(blockFile, SER_DISK, CLIENT_VERSION);
+        fseek(blockFile, 0, SEEK_END);
 
-        // Read previousBlockHash, the first 32 bytes of the expanded header
-        fseek(blockFile, -VERTHASH_MULTIPLICATION*32, SEEK_CUR);
-        filein >> FLATDATA(prevBlockHash);
-        // Rewind what we read
-        fseek(blockFile, -32, SEEK_CUR);
+        // Find common ancestor
+        while(ftell(blockFile) >= VERTHASH_MULTIPLICATION*32){
 
-        // Now find this in our blockchain
-        bool found = false;
+            // Read previousBlockHash, the first 32 bytes of the expanded header
+            fseek(blockFile, -VERTHASH_MULTIPLICATION*32, SEEK_CUR);
+            filein >> FLATDATA(prevBlockHash);
+            // Rewind what we read
+            fseek(blockFile, -32, SEEK_CUR);
 
-        CBlockIndex *idx = pindexBestHeader;
-        CBlockIndex *next = pindexBestHeader;
-        while(idx) {
-            if((idx->pprev != NULL && idx->pprev->GetBlockHash() == prevBlockHash) || (idx->pprev == NULL && prevBlockHash.IsNull())) {
-                found = true;
+            // Now find this in our blockchain
+            bool found = false;
+
+            CBlockIndex *idx = pindexBestHeader;
+            CBlockIndex *next = pindexBestHeader;
+            while(idx) {
+                if((idx->pprev != NULL && idx->pprev->GetBlockHash() == prevBlockHash) || (idx->pprev == NULL && prevBlockHash.IsNull())) {
+                    found = true;
+                    break;
+                }
+                next = idx;
+                idx = idx->pprev;
+            }
+
+            if(found) {
+
+                startIndex = next;
+                // startIndex is now equal to the first block we have to write.
+                // The file pointer is before the last block in the file, because we need to read
+                // in the last row.
+                filein >> FLATDATA(prev_row);
+                startPos = ftell(blockFile);
                 break;
             }
-            next = idx;
-            idx = idx->pprev;
         }
+        filein.fclose();
 
-        if(found) {
-
-            startIndex = next;
-            // startIndex is now equal to the first block we have to write.
-            // The file pointer is before the last block in the file, because we need to read
-            // in the last row.
-            break;
-        }
     }
-
-
 
     // Start at the tip and go back to startIndex
     CBlockIndex* idx = pindexBestHeader;
@@ -91,16 +97,9 @@ void VerthashDatFile::UpdateMiningDataFile() {
         return;
     }
 
-
-    unsigned char prev_row[VERTHASH_MULTIPLICATION][32], curr_row[VERTHASH_MULTIPLICATION][32], double_buf[64];
-
-    if(ftell(blockFile) == 0) {
-        // Initialize prev_row with constant values
-        memset(prev_row, 0, sizeof(prev_row));
-    } else {
-        // Read the last expanded block into prev_row
-        filein >> FLATDATA(prev_row);
-    }
+    blockFile = fsbridge::fopen(targetFile.c_str(),startPos > 0 ? "rb+" : "wb");
+    CAutoFile fileout(blockFile, SER_DISK, CLIENT_VERSION);
+    fseek(blockFile, startPos, SEEK_CUR);
 
     LogPrintf("Appending %d blocks to mining datafile\n", blocksToWrite.size());
 
@@ -121,7 +120,7 @@ void VerthashDatFile::UpdateMiningDataFile() {
                 sha3(double_buf, 64, curr_row[j], 32);
         }
 
-        filein << FLATDATA(curr_row);
+        fileout << FLATDATA(curr_row);
         memcpy(prev_row, curr_row, VERTHASH_MULTIPLICATION*32);
 
         blocksToWrite.pop();
@@ -130,5 +129,6 @@ void VerthashDatFile::UpdateMiningDataFile() {
 
     TruncateFile(blockFile, ftell(blockFile));
     FileCommit(blockFile);
+    fileout.fclose();
     // Done.
 }
