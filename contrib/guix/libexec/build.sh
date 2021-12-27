474e4beee7e0d01bee02bade87c377e1f4a5ed00
vertiond <35286924+vertiond@users.noreply.github.com>
2021-12-26 18:38:26 -0600

    guix: ignore libbitcoinconsensus-0.dll
    see issue #165

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -244,11 +244,11 @@ export PATH="${BASEPREFIX}/${HOST}/native/bin:${PATH}"
     (
         cd installed
 
-        case "$HOST" in
-            *mingw*)
-                mv --target-directory="$DISTNAME"/lib/ "$DISTNAME"/bin/*.dll
-                ;;
-        esac
+#        case "$HOST" in
+#            *mingw*)
+#                mv --target-directory="$DISTNAME"/lib/ "$DISTNAME"/bin/*.dll
+#                ;;
+#        esac
 
         # Prune libtool and object archives
         find . -name "lib*.la" -delete
