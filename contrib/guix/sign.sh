f8fff8d0498fcdfdbb37ae90f5b6b00d1839bd45
vertiond <vertion@protonmail.com>
2022-04-01 19:45:06 -0500

    codesigning: windows script

---
+++ contrib/guix/sign.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+VERSION="$(git rev-parse --short=12 HEAD)"
+
+cd output
+
+KEY_FILE=${KEY_FILE:-~/codesigning/vertcoin-project.p12}
+if [[ ! -f "$KEY_FILE" ]]; then
+    ls $KEY_FILE
+    echo "Make sure that $KEY_FILE exists"
+fi
+
+if ! which osslsigncode > /dev/null 2>&1; then
+    echo "Please install osslsigncode"
+fi
+
+rm -rf signed
+mkdir -p signed >/dev/null 2>&1
+
+unzip vertcoin-$VERSION-win64.zip -d signed
+cd signed/vertcoin-$VERSION/bin
+
+echo "Found $(ls *.exe | wc -w) files to sign."
+for f in $(ls *.exe); do
+    echo "Signing $f..."
+    osslsigncode sign \
+      -h sha256 \
+      -pkcs12 "$KEY_FILE" \
+      -pass "$VERTCOIN_PROJECT_KEY_PASSWORD" \
+      -n "Vertcoin-Core" \
+      -i "https://vertcoin.org/" \
+      -t "http://timestamp.digicert.com/" \
+      -in "$f" \
+      -out "../../$f"
+    ls ../../$f -lah
+done
+
+cd ../..
+rm vertcoin-$VERSION/bin/*
+mv vertcoin*.exe vertcoin-$VERSION/bin
+zip -r vertcoin-$VERSION-win64.zip vertcoin-$VERSION
+rm -r vertcoin-$VERSION
+
+echo "Signing vertcoin-$VERSION-win64-setup..."
+osslsigncode sign \
+  -h sha256 \
+  -pkcs12 "$KEY_FILE" \
+  -pass "$VERTCOIN_PROJECT_KEY_PASSWORD" \
+  -n "Vertcoin-Core" \
+  -i "https://vertcoin.org/" \
+  -t "http://timestamp.digicert.com/" \
+  -in "../vertcoin-$VERSION-win64-setup-unsigned.exe" \
+  -out "vertcoin-$VERSION-win64-setup.exe"
