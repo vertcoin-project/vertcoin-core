f5b04efde18bb20b90a28d649cbf74936f423c5d
vertion <vertion@protonmail.com>
2022-05-27 12:23:16 -0500

    vtc: startup verification
    Relevant PRs: #31, #33, #184, #187

--- src/init.cpp
+++ src/init.cpp
@@ -480,6 +480,8 @@ void SetupServerArgs(ArgsManager& argsman)
 
     argsman.AddArg("-verthash-diskonly", "Don't load Verthash's datafile into RAM. Will slow down validation significantly, but might be needed on low-memory systems.", ArgsManager::ALLOW_ANY, OptionsCategory::OPTIONS);
 
+    argsman.AddArg("-full-startup-verify", "Check the complete chain of work on startup from the Genesis block", ArgsManager::ALLOW_BOOL, OptionsCategory::OPTIONS);
+
     g_wallet_init_interface.AddWalletOptions(argsman);
 
 #if ENABLE_ZMQ
--- src/txdb.cpp
+++ src/txdb.cpp
@@ -5,6 +5,7 @@
 
 #include <txdb.h>
 
+#include <chainparams.h>
 #include <node/ui_interface.h>
 #include <pow.h>
 #include <random.h>
@@ -275,6 +276,12 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
 {
     std::unique_ptr<CDBIterator> pcursor(NewIterator());
 
+    const bool fullChainVerification = gArgs.GetBoolArg("-full-startup-verify", false);
+
+    const CChainParams& chainparams = Params();
+    MapCheckpoints checkPoints = chainparams.Checkpoints().mapCheckpoints;
+    int highestCheckpointHeight = checkPoints.rbegin()->first;
+
     pcursor->Seek(std::make_pair(DB_BLOCK_INDEX, uint256()));
 
     // Load m_block_index
@@ -299,9 +306,19 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
                 pindexNew->nStatus        = diskindex.nStatus;
                 pindexNew->nTx            = diskindex.nTx;
 
-                if (!CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, consensusParams))
-                    return error("%s: CheckProofOfWork failed: %s", __func__, pindexNew->ToString());
-
+                MapCheckpoints::iterator it = checkPoints.find(pindexNew->nHeight);
+                if(it != checkPoints.end())
+                {
+                    LogPrintf("Verifying checkpoint at height %i\n", pindexNew->nHeight);
+                    if(pindexNew->GetBlockHash() != it->second)
+                        return error("%s: Block hash mismatches checkpoint: %s\n", __func__, pindexNew->ToString());
+                }
+                if(fullChainVerification)
+                {
+                    if(pindexNew->nHeight % 10000 == 0) LogPrintf("Checking PoW for block %i\n", pindexNew->nHeight);
+                    if (!CheckProofOfWork(pindexNew->GetBlockPoWHash(), pindexNew->nBits, consensusParams))
+                        return error("%s: CheckProofOfWork failed: %s\n", __func__, pindexNew->ToString());
+                }
                 pcursor->Next();
             } else {
                 return error("%s: failed to read value", __func__);
