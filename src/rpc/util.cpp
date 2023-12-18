559b3aae32737c51b3e05b07c8a962ff2215027f
Matthew Cummings <matt@gocummings.com>
2023-12-18 16:39:08 -0500

    vtc branding: RPC

--- src/rpc/util.cpp
+++ src/rpc/util.cpp
@@ -18,7 +18,7 @@
 #include <tuple>
 
 const std::string UNIX_EPOCH_TIME = "UNIX epoch time";
-const std::string EXAMPLE_ADDRESS[2] = {"bc1q09vm5lfy0j5reeulh4x5752q25uqqvz34hufdl", "bc1q02ad21edsxd23d32dfgqqsz4vv4nmtfzuklhy3"};
+const std::string EXAMPLE_ADDRESS[2] = {"vtc1q09vm5lfy0j5reeulh4x5752q25uqqvz34hufdl", "vtc1q02ad21edsxd23d32dfgqqsz4vv4nmtfzuklhy3"};
 
 std::string GetAllOutputTypes()
 {
@@ -165,12 +165,12 @@ std::string ShellQuoteIfNeeded(const std::string& s)
 
 std::string HelpExampleCli(const std::string& methodname, const std::string& args)
 {
-    return "> bitcoin-cli " + methodname + " " + args + "\n";
+    return "> vertcoin-cli " + methodname + " " + args + "\n";
 }
 
 std::string HelpExampleCliNamed(const std::string& methodname, const RPCArgList& args)
 {
-    std::string result = "> bitcoin-cli -named " + methodname;
+    std::string result = "> vertcoin-cli -named " + methodname;
     for (const auto& argpair: args) {
         const auto& value = argpair.second.isStr()
                 ? argpair.second.get_str()
